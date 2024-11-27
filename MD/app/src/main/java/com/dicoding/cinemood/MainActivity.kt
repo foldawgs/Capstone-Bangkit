6351289d23a9f69c44c7f8afafc1730a0d60737f
AubreyR.F <aubrey.rayhan.firoz@gmail.com>
2024-11-27 11:40:39 +0700

    Login Regis

---
+++ MD/app/src/main/java/com/dicoding/cinemood/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.dicoding.cinemood
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sharedPreferences = getSharedPreferences("user_session", Context.MODE_PRIVATE)
+
+        findViewById<Button>(R.id.btnBack).setOnClickListener {
+            logoutAndNavigateToLogin()
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        logoutAndNavigateToLogin()
+    }
+
+    private fun logoutAndNavigateToLogin() {
+        sharedPreferences.edit().apply {
+            putBoolean("isLoggedIn", false)
+            remove("token")
+            apply()
+        }
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+        finish()
+    }
+}
